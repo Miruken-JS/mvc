63f03c1ddbb874c4b9c3960755d3a05fc45f3a54
michael dudley <michael.dudley@improvingenterprises.com>
2019-12-31 12:37:22 -0600

    using prereleases

--- config.js
+++ config.js
@@ -8,18 +8,18 @@ System.config({
 
   map: {
     "chai": "npm:chai@3.5.0",
-    "miruken-callback": "npm:miruken-callback@0.0.4",
-    "miruken-context": "npm:miruken-context@0.0.4",
-    "miruken-core": "npm:miruken-core@0.0.3",
-    "miruken-validate": "npm:miruken-validate@0.0.2",
+    "miruken-callback": "npm:miruken-callback@0.0.5-alpha.20",
+    "miruken-context": "npm:miruken-context@0.0.5-alpha.2",
+    "miruken-core": "npm:miruken-core@0.0.5-alpha.1",
+    "miruken-validate": "npm:miruken-validate@0.0.5-alpha.1",
     "github:jspm/nodelibs-assert@0.1.0": {
-      "assert": "npm:assert@1.4.1"
+      "assert": "npm:assert@1.5.0"
     },
-    "github:jspm/nodelibs-buffer@0.1.0": {
-      "buffer": "npm:buffer@3.6.0"
+    "github:jspm/nodelibs-buffer@0.1.1": {
+      "buffer": "npm:buffer@5.4.3"
     },
     "github:jspm/nodelibs-process@0.1.2": {
-      "process": "npm:process@0.11.9"
+      "process": "npm:process@0.11.10"
     },
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
@@ -27,52 +27,49 @@ System.config({
     "github:jspm/nodelibs-vm@0.1.0": {
       "vm-browserify": "npm:vm-browserify@0.0.4"
     },
-    "npm:assert@1.4.1": {
+    "npm:assert@1.5.0": {
       "assert": "github:jspm/nodelibs-assert@0.1.0",
-      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "buffer": "github:jspm/nodelibs-buffer@0.1.1",
+      "object-assign": "npm:object-assign@4.1.1",
       "process": "github:jspm/nodelibs-process@0.1.2",
       "util": "npm:util@0.10.3"
     },
-    "npm:buffer@3.6.0": {
-      "base64-js": "npm:base64-js@0.0.8",
-      "child_process": "github:jspm/nodelibs-child_process@0.1.0",
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "ieee754": "npm:ieee754@1.1.8",
-      "isarray": "npm:isarray@1.0.0",
-      "process": "github:jspm/nodelibs-process@0.1.2"
+    "npm:buffer@5.4.3": {
+      "base64-js": "npm:base64-js@1.3.1",
+      "ieee754": "npm:ieee754@1.1.13"
     },
     "npm:chai@3.5.0": {
       "assertion-error": "npm:assertion-error@1.0.2",
-      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "buffer": "github:jspm/nodelibs-buffer@0.1.1",
       "deep-eql": "npm:deep-eql@0.1.3",
       "process": "github:jspm/nodelibs-process@0.1.2",
       "systemjs-json": "github:systemjs/plugin-json@0.1.2",
       "type-detect": "npm:type-detect@1.0.0"
     },
     "npm:deep-eql@0.1.3": {
-      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "buffer": "github:jspm/nodelibs-buffer@0.1.1",
       "type-detect": "npm:type-detect@0.1.1"
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
     },
-    "npm:miruken-callback@0.0.4": {
-      "miruken-core": "npm:miruken-core@0.0.3"
+    "npm:miruken-callback@0.0.5-alpha.20": {
+      "miruken-core": "npm:miruken-core@0.0.5-alpha.1"
     },
-    "npm:miruken-context@0.0.4": {
-      "miruken-callback": "npm:miruken-callback@0.0.4",
-      "miruken-core": "npm:miruken-core@0.0.3"
+    "npm:miruken-context@0.0.5-alpha.2": {
+      "miruken-callback": "npm:miruken-callback@0.0.5-alpha.20",
+      "miruken-core": "npm:miruken-core@0.0.5-alpha.1"
     },
-    "npm:miruken-core@0.0.3": {
-      "reflect-metadata": "npm:reflect-metadata@0.1.12"
+    "npm:miruken-core@0.0.5-alpha.1": {
+      "reflect-metadata": "npm:reflect-metadata@0.1.13"
     },
-    "npm:miruken-validate@0.0.2": {
-      "miruken-callback": "npm:miruken-callback@0.0.4",
-      "miruken-context": "npm:miruken-context@0.0.4",
-      "miruken-core": "npm:miruken-core@0.0.3",
+    "npm:miruken-validate@0.0.5-alpha.1": {
+      "miruken-callback": "npm:miruken-callback@0.0.5-alpha.20",
+      "miruken-context": "npm:miruken-context@0.0.5-alpha.2",
+      "miruken-core": "npm:miruken-core@0.0.5-alpha.1",
       "validate.js": "npm:validate.js@0.10.0"
     },
-    "npm:process@0.11.9": {
+    "npm:process@0.11.10": {
       "assert": "github:jspm/nodelibs-assert@0.1.0",
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "vm": "github:jspm/nodelibs-vm@0.1.0"
